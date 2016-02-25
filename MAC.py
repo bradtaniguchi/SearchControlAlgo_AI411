7c72af9c03d2bd9d8603cf7b515b9afd8efdfdb2
braddtaniguchi <bradtaniguchi@gmail.com>
2016-02-24 16:38:40 -0800

    Changed main imports for Problem State, added more classes and functions in MAC

--- MAC.py
+++ MAC.py
@@ -4,6 +4,20 @@
 __author__ = 'brad'
 
 
+class Stack:
+    def __init__(self):
+        self.items= []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+
 class ProblemState():
     def __init__(self, cannLeft, missLeft, boat, cannRight, missRight):
         self.cannLeft = cannLeft
@@ -11,3 +25,29 @@ class ProblemState():
         self.boat = boat
         self.cannRight = cannRight
         self.missRight = missRight
+        self.parent = None
+
+    def is_win(self):
+        if self.cannLeft == 0 and self.missLeft == 0:
+            return True # WE WIN
+        else:
+            return False
+
+    def is_valid(self):
+        if self.missLeft >= 0 and self.missRight >= 0 \
+            and self.cannLeft >= 0 and self.cannRight >= 0 \
+            and (self.missLeft == 0 or self.missLeft >= self.cannLeft) \
+            and (self.missRight == 0 or self.missRight >= self.cannRight):
+                return True
+        else:
+            return False
+
+
+def child(cur_state):
+    children = []
+    if cur_state.boat == 'left':
+        print("Poop")
+    else:  # boat is on the rightside
+        print("Poop")
+    return children
+
--- SearchControlAlgo.py
+++ SearchControlAlgo.py
@@ -2,13 +2,15 @@
 # Bradley Taniguchi
 # Search Control Algo for CSC 411 AI class
 
-from DFS import Tree
+from MAC import Stack
+from MAC import ProblemState
 
 __author__ = 'Bradley Taniguchi'
 
 
 def main():
     print("IN MAIN")
+    mystack = Stack
 
 if __name__ == "__main__":
     main()
